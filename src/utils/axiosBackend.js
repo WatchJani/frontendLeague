8a3bbda42a0ac2013768efff8b1ed7e1a390a51a
Janko <45310863+WatchJani@users.noreply.github.com>
2022-06-21 04:18:38 -0700

    Update axiosBackend.js

--- src/utils/axiosBackend.js
+++ src/utils/axiosBackend.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosBackend = axios.create({
   baseURL: process.env.NODE_ENV
     ? 'http://localhost:5000/api/v1/'
-    : 'https://league.savokos.com/api/v1/',
+    : 'https://8080-d977dd38-fc04-4404-99ac-a8a5cc57d6ef.cs-europe-west1-onse.cloudshell.dev/api/v1/',
 });
 
 export default axiosBackend;
