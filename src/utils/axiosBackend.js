595ac012d2018a276a65bc55db19371d26fedee6
Janko <45310863+WatchJani@users.noreply.github.com>
2022-06-21 05:36:10 -0700

    Update axiosBackend.js

--- src/utils/axiosBackend.js
+++ src/utils/axiosBackend.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosBackend = axios.create({
-  baseURL: process.env.NODE_ENV
-    ? 'https://8080-d977dd38-fc04-4404-99ac-a8a5cc57d6ef.cs-europe-west1-onse.cloudshell.dev/api/v1/'
+  baseURL: process.env.NODE_ENV === "development"
+    ? 'http://localhost:5000/api/v1/'
     : 'https://8080-d977dd38-fc04-4404-99ac-a8a5cc57d6ef.cs-europe-west1-onse.cloudshell.dev/api/v1/',
 });
 
