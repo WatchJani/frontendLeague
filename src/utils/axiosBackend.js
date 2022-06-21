82c5408d0eed391589a1ce6282bd19fd852ef5d6
Janko <45310863+WatchJani@users.noreply.github.com>
2022-06-21 06:29:16 -0700

    Update axiosBackend.js

--- src/utils/axiosBackend.js
+++ src/utils/axiosBackend.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosBackend = axios.create({
   baseURL: process.env.NODE_ENV === "development"
     ? 'http://localhost:5000/api/v1/'
-    : 'https://8080-d977dd38-fc04-4404-99ac-a8a5cc57d6ef.cs-europe-west1-onse.cloudshell.dev/api/v1/',
+    : 'https://score-tracker-league.herokuapp.com/api/v1/',
 });
 
 export default axiosBackend;
