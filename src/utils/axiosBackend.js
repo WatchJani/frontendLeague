7dc7fefb4d5cd959fbc48362a64575d74bdea359
WatchJani <jankokondic85@gmail.com>
2022-06-25 13:40:24 +0200

    link

--- src/utils/axiosBackend.js
+++ src/utils/axiosBackend.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosBackend = axios.create({
   baseURL: process.env.NODE_ENV === "development"
     ? 'http://localhost:5000/api/v1/'
-    : 'master--scoretracker-league.netlify.app/api/v1/',
+    : 'https://master--scoretracker-league.netlify.app/api/v1/',
 });
 
 export default axiosBackend;
